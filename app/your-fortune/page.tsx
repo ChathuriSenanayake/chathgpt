c92a66f25b7a3382346a231b810f4b0a475c9c51
Chathuri Senanayake <chathuri@lizard-apps.com>
2025-10-25 16:40:00 +0800

    feat: fix linter errors

--- app/your-fortune/page.tsx
+++ app/your-fortune/page.tsx
@@ -1,154 +1,71 @@
-'use client';
-import { useParams, useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
-import { motion, useAnimation } from 'framer-motion';
-import data from '../../utils/fortune.json';
+import { Suspense, } from "react";
+import YourFortuneResults from "./__components/Fortune";
 
-export default function YourFortuneResults() {
-    const params = useSearchParams();
-    const [step, setStep] = useState(1);
-    const textAnimateControls = useAnimation();
-    const nameanimateControls = useAnimation();
 
-    const fortune = useMemo(() => {
-        try {
-            const name = params.get("name")
-            console.log("name:", name && decodeURIComponent(name).trim());
-            const hasKey = name && data.find(item => decodeURIComponent(name).trim().toLowerCase() === item.name.toLowerCase());
-            if (!hasKey) {
-                throw new Error("name not found")
-            }
-            return hasKey.fortune
-        } catch (error) {
-            return "Your future is still being written. Check again a year later!";
-        }
+export default function YourFortuneResultsPage() {
+    // const params = useSearchParams();
+    // const [step, setStep] = useState(1);
+    // const textAnimateControls = useAnimation();
+    // const nameanimateControls = useAnimation();
 
-    }, [params])
+    // const fortune = useMemo(() => {
+    //     try {
+    //         const name = params.get("name")
+    //         console.log("name:", name && decodeURIComponent(name).trim());
+    //         const hasKey = name && data.find(item => decodeURIComponent(name).trim().toLowerCase() === item.name.toLowerCase());
+    //         if (!hasKey) {
+    //             throw new Error("name not found")
+    //         }
+    //         return hasKey.fortune
+    //     } catch (error) {
+    //         return "Your future is still being written. Check again a year later!";
+    //     }
 
-    useEffect(() => {
-        let timer: NodeJS.Timeout;
-        switch (step) {
-            case 1:
-                timer = setTimeout(() => {
-                    setStep(2);
-                }, 8000);
-                break;
-            case 2:
-                timer = setTimeout(() => {
-                    setStep(3);
-                }, 2000);
-                nameanimateControls.start({
-                    opacity: [0, 1],
-                    scale: [0, 1],
-                    rotate: [0, 360],
-                    transition: { duration: 2 },
-                });
-                break;
-            case 3:
-                nameanimateControls.start({
-                    y: [0, -50],
+    // }, [params])
 
-                    transition: { duration: 2 },
-                });
-                textAnimateControls.start({
-                    y: [0, -10],
-                    opacity: [0, 1],
-                    transition: { duration: 2 },
-                });
-                break;
-            default:
-                break;
-        }
+    // useEffect(() => {
+    //     let timer: NodeJS.Timeout;
+    //     switch (step) {
+    //         case 1:
+    //             timer = setTimeout(() => {
+    //                 setStep(2);
+    //             }, 8000);
+    //             break;
+    //         case 2:
+    //             timer = setTimeout(() => {
+    //                 setStep(3);
+    //             }, 2000);
+    //             nameanimateControls.start({
+    //                 opacity: [0, 1],
+    //                 scale: [0, 1],
+    //                 rotate: [0, 360],
+    //                 transition: { duration: 2 },
+    //             });
+    //             break;
+    //         case 3:
+    //             nameanimateControls.start({
+    //                 y: [0, -50],
 
-        return () => clearTimeout(timer);
-    }, [step]);
+    //                 transition: { duration: 2 },
+    //             });
+    //             textAnimateControls.start({
+    //                 y: [0, -10],
+    //                 opacity: [0, 1],
+    //                 transition: { duration: 2 },
+    //             });
+    //             break;
+    //         default:
+    //             break;
+    //     }
+
+    //     return () => clearTimeout(timer);
+    // }, [step]);
 
 
 
     return (
-        <div className="min-h-screen flex flex-col font-sans overflow-clip">
-            {/* Main Content */}
-            <main className="flex-1 flex flex-col items-center justify-center p-4">
-
-                {step === 1 && (
-                    <div className="gap-10">
-                        <motion.div
-                            initial={{ y: 0, opacity: 0 }}
-                            aria-hidden
-                            animate={{ y: [0, -20, 0], opacity: [0, 1, 0] }}
-                            transition={{ duration: 8, }}
-                            className=" text-white text-2xl text-center">
-                            {"Gazing into the crystal ball..."}
-                        </motion.div>
-
-                        <motion.div
-                            initial={{ scale: 0 }}
-                            animate={{ scale: [0, 1, 0], transition: { duration: 8 } }}
-                            className="w-96 h-96 rounded-full  bg-white/90 blur-2xl"
-                        >
-                        </motion.div>
-
-                    </div>
-                )}
-
-                <motion.div
-                    initial={{ rotate: 0, scale: 0 }}
-                    // animate={{ rotate: [0, 360], scale: [0, 1], transition: { duration: 2 } }}
-                    animate={nameanimateControls}
-                    className="text-white text-2xl"
-                >
-                    {"Dear " + params?.get("name") || "Seeker of Truth" + ","}
-
-                </motion.div>
-                <motion.div
-                    aria-hidden
-                    initial={{ opacity: 0 }}
-                    animate={textAnimateControls}
-                    transition={{ duration: 3, }}
-                    className=" text-white text-xl max-w-xl text-center"
-                >
-                    {fortune}
-
-                </motion.div>
-                {/* {step === 3 && (
-                    <>
-                        <div className="flex flex-row">
-                            <motion.div
-                                initial={{ y: 0, opacity: 0 }}
-                                aria-hidden
-                                animate={{ y: [20, -20, 0], opacity: [0, 1, 0] }}
-                                transition={{ duration: 8, }}
-                                className=" text-white text-2xl">
-                            </motion.div>
-
-                        </div>
-                        <motion.div
-                            initial={{ y: 0 }}
-                            animate={{ y: [0, -20], transition: { duration: 2 } }}
-
-                            className="text-white text-2xl"
-                        >
-                            {"Dear," + params?.get("name") || "Seeker of Truth,"}
-
-                        </motion.div>
-                        <motion.div
-                            initial={{ y: 20, opacity: 0 }}
-                            aria-hidden
-                            animate={{ y: [20, 0], opacity: [0, 1] }}
-                            transition={{ delay: 2, duration: 3, }}
-                            className=" text-white text-2xl mt-4 max-w-xl text-center"
-                        >
-                            {"The path ahead is filled with opportunities and challenges. Embrace change, trust your instincts, and remember that every step you take shapes your destiny. Fortune favors the bold!"}
-
-                        </motion.div>
-                    </>
-                )} */}
-            </main>
-
-            {/* Footer
-            <footer className="flex text-xs  items-center justify-center p-4 text-white">
-                <div>Created by Chath_S</div>
-            </footer> */}
-        </div>
+        <Suspense fallback={<div className="min-h-screen flex flex-col font-sans overflow-clip" />}>
+            <YourFortuneResults />
+        </Suspense>
     );
 }
